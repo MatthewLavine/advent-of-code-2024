c6d90acbd1a37652bd7f0197d1d583e0e5a982c0
Matthew Lavine <matt@matthewlavine.net>
2024-12-01 13:44:37 -0500

    Day 1 solution

--- day1/day1.go
+++ day1/day1.go
@@ -4,19 +4,154 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"regexp"
+	"slices"
+	"sort"
+	"strconv"
+	"strings"
 )
 
-const inputFile = "demo.txt"
+const (
+	inputFile = "input.txt"
+)
+
+var (
+	numberRegex = regexp.MustCompile("^[0-9]*$")
+)
 
 func main() {
-	input := readInputFile()
-	fmt.Printf("%s\n", input)
-}
-
-func readInputFile() string {
-	bytes, err := os.ReadFile(inputFile)
+	input, err := readInputFile()
 	if err != nil {
 		log.Fatal(err)
 	}
-	return string(bytes)
+
+	left, right, err := parseInput(input)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	distance, err := calculateListDistance(left, right)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Distance: %d\n", distance)
+
+	similar, err := calculateListSimilarity(left, right)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Similarity: %d\n", similar)
+}
+
+func readInputFile() (string, error) {
+	bytes, err := os.ReadFile(inputFile)
+	if err != nil {
+		return "", err
+	}
+	return string(bytes), nil
+}
+
+func parseInput(input string) ([]int, []int, error) {
+	var left []int
+	var right []int
+	for i, line := range strings.Split(input, "\n") {
+		if line == "" {
+			continue
+		}
+
+		var found []string
+
+		for _, char := range strings.Fields(line) {
+
+			if numberRegex.MatchString(char) {
+				found = append(found, char)
+			}
+		}
+
+		if len(found) != 2 {
+			return nil, nil, fmt.Errorf("not enough numbers found in line %d: %s\n found %s", i, line, found)
+		}
+
+		l, err := strconv.Atoi(found[0])
+		if err != nil {
+			return nil, nil, err
+		}
+		r, err := strconv.Atoi(found[1])
+		if err != nil {
+			return nil, nil, err
+		}
+
+		left = append(left, l)
+		right = append(right, r)
+	}
+	return left, right, nil
+}
+
+func calculateListDistance(left, right []int) (int, error) {
+	if len(left) != len(right) {
+		return -1, fmt.Errorf("left and right lists are not the same length")
+	}
+
+	diffs := make([]int, len(left))
+
+	for i := 0; i < len(left); i++ {
+		l, err := nthSmallest(i, left)
+		if err != nil {
+			return -1, err
+		}
+		r, err := nthSmallest(i, right)
+		if err != nil {
+			return -1, err
+		}
+		diff := r - l
+		if diff < 0 {
+			diff = -diff
+		}
+		diffs[i] = diff
+	}
+
+	distance := 0
+	for _, diff := range diffs {
+		distance += diff
+	}
+
+	return distance, nil
+}
+
+func nthSmallest(n int, arr []int) (int, error) {
+	if n > len(arr) {
+		return -1, fmt.Errorf("n is greater than the length of the array")
+	}
+	copy := slices.Clone(arr)
+	sort.Ints(copy)
+	return copy[n], nil
+}
+
+func calculateListSimilarity(left, right []int) (int, error) {
+	if len(left) != len(right) {
+		return -1, fmt.Errorf("left and right lists are not the same length")
+	}
+
+	similarities := make([]int, len(left))
+
+	for i := 0; i < len(left); i++ {
+		similarities[i] = left[i] * count(left[i], right)
+	}
+
+	sum := 0
+	for _, similarity := range similarities {
+		sum += similarity
+	}
+
+	return sum, nil
+}
+
+func count(n int, arr []int) int {
+	count := 0
+	for _, i := range arr {
+		if i == n {
+			count++
+		}
+	}
+	return count
 }
