e28707567498e65d8500b695bfdf4f5c4c71a6e1
Matthew Lavine <matt@matthewlavine.net>
2024-12-03 17:20:11 -0500

    day3 skeleton

---
+++ day3/day3.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"runtime/pprof"
+)
+
+const (
+	demoInputFile = "demo.txt"
+	inputFile     = "input.txt"
+)
+
+var (
+	demo        = flag.Bool("demo", false, "Use demo input")
+	enablePprof = flag.Bool("pprof", false, "Enable pprof")
+)
+
+func main() {
+	flag.Parse()
+
+	if *enablePprof {
+		f, err := os.Create("profile.prof")
+		if err != nil {
+			panic(err)
+		}
+		defer f.Close()
+		if err := pprof.StartCPUProfile(f); err != nil {
+			panic(err)
+		}
+		defer pprof.StopCPUProfile()
+	}
+
+	path := inputFile
+	if *demo {
+		path = demoInputFile
+	}
+
+	fmt.Println(path)
+}
