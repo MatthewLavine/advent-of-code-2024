58efa16c35b3dc5af8999de07c3a1d57b1621030
Matthew Lavine <matt@matthewlavine.net>
2024-12-04 18:11:01 -0500

    Working for demo

--- day4/day4.go
+++ day4/day4.go
@@ -49,7 +49,9 @@ func main() {
 		log.Fatal(err)
 	}
 
-	processMatrix(matrix)
+	count := processMatrix(matrix)
+
+	fmt.Printf("Xmas count: %d\n", count)
 }
 
 func readInputFile(file string) (string, error) {
@@ -68,11 +70,82 @@ func parseInput(input string) ([][]string, error) {
 	return matrix, nil
 }
 
-func processMatrix(matrix [][]string) {
-	for _, row := range matrix {
-		for _, col := range row {
-			fmt.Printf("%s ", col)
+func processMatrix(matrix [][]string) int {
+	count := 0
+	printMatrix(matrix)
+	for i, row := range matrix {
+		for j := range row {
+			count += startsXmas(matrix, i, j)
 		}
-		fmt.Println()
 	}
+	return count
+}
+
+func printMatrix(matrix [][]string) {
+	for _, row := range matrix {
+		fmt.Println(strings.Join(row, " "))
+	}
+	fmt.Println()
+}
+
+// ew
+func startsXmas(matrix [][]string, row, col int) int {
+	found := 0
+	// Left to right
+	if col+3 < len(matrix[row]) {
+		if matrix[row][col] == "X" && matrix[row][col+1] == "M" && matrix[row][col+2] == "A" && matrix[row][col+3] == "S" {
+			fmt.Printf("Found left to right xmas at %d, %d\n", row+1, col+1)
+			found++
+		}
+	}
+	// Right to left
+	if col-3 >= 0 {
+		if matrix[row][col] == "X" && matrix[row][col-1] == "M" && matrix[row][col-2] == "A" && matrix[row][col-3] == "S" {
+			fmt.Printf("Found right to left xmas at %d, %d\n", row+1, col+1)
+			found++
+		}
+	}
+	// Top to bottom
+	if row+3 < len(matrix) {
+		if matrix[row][col] == "X" && matrix[row+1][col] == "M" && matrix[row+2][col] == "A" && matrix[row+3][col] == "S" {
+			fmt.Printf("Found top to bottom xmas at %d, %d\n", row+1, col+1)
+			found++
+		}
+	}
+	// Bottom to top
+	if row-3 >= 0 {
+		if matrix[row][col] == "X" && matrix[row-1][col] == "M" && matrix[row-2][col] == "A" && matrix[row-3][col] == "S" {
+			fmt.Printf("Found bottom to top xmas at %d, %d\n", row+1, col+1)
+			found++
+		}
+	}
+	// Diagonal top left to bottom right
+	if col+3 < len(matrix[row]) && row+3 < len(matrix) {
+		if matrix[row][col] == "X" && matrix[row+1][col+1] == "M" && matrix[row+2][col+2] == "A" && matrix[row+3][col+3] == "S" {
+			fmt.Printf("Found diagonal top left to bottom right xmas at %d, %d\n", row+1, col+1)
+			found++
+		}
+	}
+	// Diagonal bottom right to top left
+	if col-3 >= 0 && row-3 >= 0 {
+		if matrix[row][col] == "X" && matrix[row-1][col-1] == "M" && matrix[row-2][col-2] == "A" && matrix[row-3][col-3] == "S" {
+			fmt.Printf("Found diagonal bottom right to top left xmas at %d, %d\n", row+1, col+1)
+			found++
+		}
+	}
+	// Diagonal top right to bottom left
+	if col-3 >= 0 && row+3 < len(matrix) {
+		if matrix[row][col] == "X" && matrix[row+1][col-1] == "M" && matrix[row+2][col-2] == "A" && matrix[row+3][col-3] == "S" {
+			fmt.Printf("Found diagonal top right to bottom left xmas at %d, %d\n", row+1, col+1)
+			found++
+		}
+	}
+	// Diagonal bottom left to top right
+	if col+3 < len(matrix[row]) && row-3 >= 0 {
+		if matrix[row][col] == "X" && matrix[row-1][col+1] == "M" && matrix[row-2][col+2] == "A" && matrix[row-3][col+3] == "S" {
+			fmt.Printf("Found diagonal bottom left to top right xmas at %d, %d\n", row+1, col+1)
+			found++
+		}
+	}
+	return found
 }
