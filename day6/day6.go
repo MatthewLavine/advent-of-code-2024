6cf5d0dd08e39b75ffe8f73183b6e6610ddb6f95
Matthew Lavine <matt@matthewlavine.net>
2024-12-06 18:23:23 -0500

    Day6 template

---
+++ day6/day6.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"runtime/pprof"
+)
+
+const (
+	demoInputFile = "demo.txt"
+	inputFile     = "input.txt"
+)
+
+var (
+	demo        = flag.Bool("demo", false, "Use demo input")
+	enablePprof = flag.Bool("pprof", false, "Enable pprof")
+	verbose     = flag.Bool("v", false, "Enable verbose output")
+)
+
+type rule struct {
+	l, r int
+}
+
+func main() {
+	flag.Parse()
+
+	if *enablePprof {
+		f, err := os.Create("profile.prof")
+		if err != nil {
+			panic(err)
+		}
+		defer f.Close()
+		if err := pprof.StartCPUProfile(f); err != nil {
+			panic(err)
+		}
+		defer pprof.StopCPUProfile()
+	}
+
+	path := inputFile
+	if *demo {
+		path = demoInputFile
+	}
+
+	input, err := readInputFile(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(input)
+}
+
+func readInputFile(file string) (string, error) {
+	bytes, err := os.ReadFile(file)
+	if err != nil {
+		return "", err
+	}
+	return string(bytes), nil
+}
