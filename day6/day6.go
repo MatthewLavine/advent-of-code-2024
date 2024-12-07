472053690efe00783e7e54ead701bb3f4db3e787
Matthew Lavine <matt@matthewlavine.net>
2024-12-06 20:50:00 -0500

    Working demo

--- day6/day6.go
+++ day6/day6.go
@@ -7,6 +7,7 @@ import (
 	"os"
 	"runtime/pprof"
 	"strings"
+	"time"
 )
 
 const (
@@ -85,12 +86,87 @@ func printMap(m [][]string) {
 	for _, row := range m {
 		fmt.Println(strings.Join(row, " "))
 	}
+	fmt.Println()
 }
 
-func traverse(m [][]string, col, row int) int {
+func traverse(m [][]string, startingCol, startingRow int) int {
 	uniquePositions := 0
+
+	currCol := startingCol
+	currRow := startingRow
+	direction := "up"
+
+	printMap(m)
+
+	for {
+		nextRow, nextCol := nextPos(direction, currRow, currCol)
+		if *verbose {
+			fmt.Printf("Curr: %d, %d, Next: %d, %d, Uniq: %d\n", currRow, currCol, nextRow, nextCol, uniquePositions)
+			printMap(m)
+		}
+		if nextRow < 0 || nextRow >= len(m) || nextCol < 0 || nextCol >= len(m[0]) {
+			uniquePositions++
+			break
+		}
+		if m[nextRow][nextCol] == "#" {
+			direction = turn(direction)
+			continue
+		}
+		if m[nextRow][nextCol] != "X" {
+			uniquePositions++
+		}
+		m[currRow][currCol] = "X"
+		currCol = nextCol
+		currRow = nextRow
+		m[nextRow][nextCol] = charForDirection(direction)
+		if *verbose {
+			time.Sleep(100 * time.Millisecond)
+		}
+	}
 
 	printMap(m)
 
 	return uniquePositions
+}
+
+func nextPos(direction string, currRow, currCol int) (int, int) {
+	switch direction {
+	case "up":
+		return currRow - 1, currCol
+	case "down":
+		return currRow + 1, currCol
+	case "left":
+		return currRow, currCol - 1
+	case "right":
+		return currRow, currCol + 1
+	}
+	return 0, 0
+}
+
+func turn(direction string) string {
+	switch direction {
+	case "up":
+		return "right"
+	case "right":
+		return "down"
+	case "down":
+		return "left"
+	case "left":
+		return "up"
+	}
+	return ""
+}
+
+func charForDirection(direction string) string {
+	switch direction {
+	case "up":
+		return "^"
+	case "down":
+		return "v"
+	case "left":
+		return "<"
+	case "right":
+		return ">"
+	}
+	return ""
 }
