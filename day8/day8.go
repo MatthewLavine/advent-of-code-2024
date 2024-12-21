21fb8d0464392a63866f679428a452db134cd0b0
Matthew Lavine <matt@matthewlavine.net>
2024-12-21 09:34:48 -0500

    Parsing, antenna identification

--- day8/day8.go
+++ day8/day8.go
@@ -5,7 +5,9 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"regexp"
 	"runtime/pprof"
+	"strings"
 )
 
 const (
@@ -17,6 +19,8 @@ var (
 	demo        = flag.Bool("demo", false, "Use demo input")
 	enablePprof = flag.Bool("pprof", false, "Enable pprof")
 	verbose     = flag.Bool("v", false, "Enable verbose output")
+
+	antennaRegex = regexp.MustCompile(`[A-Za-z\d]`)
 )
 
 func main() {
@@ -44,7 +48,16 @@ func main() {
 		log.Fatal(err)
 	}
 
-	fmt.Println(input)
+	data := parseMap(input)
+
+	printMap(data)
+
+	count, err := findAntinodes(data)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("Antinode Count: %d\n", count)
 }
 
 func readInputFile(file string) (string, error) {
@@ -53,4 +66,32 @@ func readInputFile(file string) (string, error) {
 		return "", err
 	}
 	return string(bytes), nil
+}
+
+func parseMap(input string) [][]string {
+	rows := strings.Split(input, "\n")
+	m := make([][]string, len(rows))
+	for i, row := range rows {
+		m[i] = strings.Split(row, "")
+	}
+	return m
+}
+
+func printMap(m [][]string) {
+	for _, row := range m {
+		fmt.Println(strings.Join(row, " "))
+	}
+	fmt.Println()
+}
+
+func findAntinodes(m [][]string) (int, error) {
+	antinodes := 0
+	for i, row := range m {
+		for j, col := range row {
+			if antennaRegex.MatchString(col) {
+				fmt.Printf("Row: %d, Col: %d, Value: %s\n", i, j, col)
+			}
+		}
+	}
+	return antinodes, nil
 }
